1321162c95ba758f88248faef393f450c89c8386
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-20 13:42:34 +0300

    added: ES Lint and fixed auto-fixable probelms.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import InputBar from './InputBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <div className='app-container'>
+        <Header />
+        <InputBar />
+      </div>
     );
   }
 }
---
+++ src/InputBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class InputBar extends Component {
+
+	render() {
+		return (
+			<input placeholder='What needs to be done?' className='app-input'/>
+		);
+	}
+}
+
+export default InputBar;
