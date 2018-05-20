5553f86d543d916306bd04269c3a0978eb2857e0
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-20 14:07:41 +0300

    fixed: linter errors

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import InputBar from './InputBar';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='app-container'>
-        <Header />
-        <InputBar />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='app-container'>
+				<Header />
+				<InputBar />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Header extends Component {
-    render() {
-        return (
-            <header className='app-header'>
-                <h1>todo</h1>
-            </header>
-        );
-    }
+	render() {
+		return (
+			<header className='app-header'>
+				<h1>todo</h1>
+			</header>
+		);
+	}
 }
 
 export default Header;
