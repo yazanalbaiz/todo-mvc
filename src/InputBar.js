b88ba06d05fe2bb7a13d26c0569261c108586dbe
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-21 14:41:38 +0300

    removed: Useless con in inputbar.js

--- src/InputBar.js
+++ src/InputBar.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class InputBar extends Component {
-	constructor(props) {
-		super(props);
-    }
-    
     handleInput = e => {
         e.preventDefault();
         this.props.onAdd(e.target.task.value);
