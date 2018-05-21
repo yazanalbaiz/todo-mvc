df9e88397d9d3197551f294791102b67e5396028
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-21 15:38:11 +0300

    added: Nav is better looking now

--- src/Nav.js
+++ src/Nav.js
@@ -5,18 +5,18 @@ class Nav extends Component {
 	render() {
 		return (
 			<div>
-				<nav>
-					<ul>
-						<li><Link to='/'>All</Link></li>
-						<li><Link to='/active'>Active</Link></li>
-						<li><Link to='/completed'>Completed</Link></li>
+				<nav className='nav'>
+					<ul className='menu'>
+						<li className='menu-item'><Link to='/'>All</Link></li>
+						<li className='menu-item'><Link to='/active'>Active</Link></li>
+						<li className='menu-item'><Link to='/completed'>Completed</Link></li>
+						{this.props.checkCompleted() && (
+							<li className='menu-item' onClick={this.props.clearCompleted}>
+								<span>Clear Completed</span>
+							</li>
+						)}
 					</ul>
 				</nav>
-				{this.props.checkCompleted() && (
-					<button onClick={this.props.clearCompleted}>
-                        Clear Completed
-					</button>
-				)}
 			</div>
 		);
 	}
