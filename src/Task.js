e84d9e1ae107da6801735ae0fb5a64c6b1386b2c
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-21 10:54:44 +0300

    added: Base list functionality Task are added but are volitile

--- src/Task.js
+++ src/Task.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Task extends Component {
-    
+
+	render() {
+		return(
+			<ol>
+				{this.props.tasks.map((task, index) => 
+					<li className='task' key={index}>{task}</li>
+				)}
+			</ol>
+		);
+	}
 }
 
 Task.propTypes = {
