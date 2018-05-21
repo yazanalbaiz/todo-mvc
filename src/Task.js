9b2aa3a95dfd63ffeb8fa15b3957d04853140bf0
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-21 10:51:09 +0300

    added: Task component prop-types

--- src/Task.js
+++ src/Task.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Task extends Component {
-
+    
 }
+
+Task.propTypes = {
+	tasks: PropTypes.array
+};
 
 export default Task;
