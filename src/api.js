b998814fb6fc92a24286026604f83ae4d38c8ce9
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-23 10:39:48 +0300

    added: getAll() from json endpoint

--- src/api.js
+++ src/api.js
@@ -1,1 +1,7 @@
 const endpoint = 'https://www.jsonstore.io/c2161c0489c4f6a3d6a37fa92403b3265663b403ea363364cd7ecade73f39254';
+
+export const getAll = () => (
+	fetch(`${endpoint}/`)
+		.then(res => res.json())
+		.then(data => data.tasks)
+);
