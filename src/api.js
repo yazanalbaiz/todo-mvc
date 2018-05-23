91ddf31ae2a3f9fe4afd53b13183c432fb25c8c8
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-23 11:01:22 +0300

    added: add() method as POST request

--- src/api.js
+++ src/api.js
@@ -3,5 +3,15 @@ const endpoint = 'https://www.jsonstore.io/c2161c0489c4f6a3d6a37fa92403b3265663b
 export const getAll = () => (
 	fetch(`${endpoint}/`)
 		.then(res => res.json())
-		.then(data => data.tasks)
+		.then(data => data.result.tasks)
+);
+
+export const add = (body) => (
+	fetch(`${endpoint}/tasks/${body.id}`, {
+		headers: {
+			'Content-type': 'application/json'
+		},
+		method: 'POST',
+		body: JSON.stringify(body),
+	}).then(res => res.json())
 );
