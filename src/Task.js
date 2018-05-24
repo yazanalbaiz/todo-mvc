abcc4ab62ea712e94aa7e11861e541225d07133e
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-24 14:41:52 +0300

    added: edit task name function in Task.js

--- src/Task.js
+++ src/Task.js
@@ -3,6 +3,35 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 class Task extends Component {
+	handleClick = (e, task) => {
+		let target;
+
+		if(e.target.className.includes('task')) {
+			target = e.target;
+		} else {
+			target = e.target.parentElement;
+		}
+
+		target.innerHTML = '';
+
+		let newForm = document.createElement('form');
+		let newInput = document.createElement('input');
+		
+		newForm.addEventListener('submit', (e) => {
+			e.preventDefault();
+			let newTask = {
+				...task,
+				name: e.target.newName.value
+			} 
+			this.props.onSubmit(newTask);
+		});
+
+		newInput.value = task.name;
+		newInput.name = 'newName';
+
+		newForm.appendChild(newInput);
+		target.appendChild(newForm);
+	}
 	render() {
 		return(
 			<ol>
@@ -10,6 +39,7 @@ class Task extends Component {
 					<li 
 						className={task.className} 
 						key={index}
+						onDoubleClick={(e)=>this.handleClick(e, task)}
 					>
 						<i
 							className='far fa-circle'
